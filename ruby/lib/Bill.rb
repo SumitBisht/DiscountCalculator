f3a6147844c80f220c0906f6583d95470a9afa5b
Sumit Bisht <sumitbisht1987@gmail.com>
2013-05-29 23:13:50 +0530

    provided for the desired use case and wrote its tests partially

--- ruby/lib/Bill.rb
+++ ruby/lib/Bill.rb
@@ -1,7 +1,8 @@
 class Bill
-	def initialize
+	def initialize(user=nil)
 		@grocery_costs = 0
 		@other_costs = 0
+		@user= user
 	end
 	def add_item(item, category, quantity, price)
 		@other_costs += quantity * price if category != "groceries"
@@ -13,5 +14,15 @@ class Bill
 	end
 	def non_grocery_cost
 		@other_costs
+	end
+
+	def net_payable_amount
+		if @user != nil
+			percentage_disc = non_grocery_cost() * @user.discount()/100
+			total_cost = cost() - percentage_disc
+			total_cost = total_cost - (total_cost/100)*5
+			return total_cost
+		end
+		cost()
 	end
 end
--- ruby/test/TestComputedDiscounts.rb
+++ ruby/test/TestComputedDiscounts.rb
@@ -1,7 +1,9 @@
 require 'test/unit'
+require '../lib/Bill'
+require '../lib/User'
 # Reusing contents created earlier
-require 'TestApplicableDiscount'
-require 'TestBillDetails'
+# require './TestApplicableDiscounts'
+# require './TestBillDetails'
 
 class TestComputedDiscounts < Test::Unit::TestCase
 
@@ -10,6 +12,14 @@ class TestComputedDiscounts < Test::Unit::TestCase
 	def test_old_customer_discounts
 	end
 	def test_employee_discounts
+		bill = Bill.new(User.new('employee', Time.new(2012,4,3)))
+		bill.add_item('bat', 'sports', 2, 50)
+		bill.add_item('ball', 'sports', 5, 8)
+		bill.add_item('stumps', 'sports', 6, 15)
+		bill.add_item('bails', 'sports', 4, 5)
+		assert_not_equal(250, bill.net_payable_amount)
+		assert_equal(170, bill.net_payable_amount)
+
 	end
 	def test_affiliate_discounts
 	end

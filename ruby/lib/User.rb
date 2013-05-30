678c6864731930bcca18a2f804ad95d468138a2b
sumitbisht4u <sumitbisht1987@gmail.com>
2013-05-30 10:09:27 +0530

    updated the field in user to properly reflect its purpose

--- ruby/lib/User.rb
+++ ruby/lib/User.rb
@@ -1,13 +1,13 @@
 class User
-	def initialize(name, time)
-		@name = name
+	def initialize(type, time)
+		@type = type
 		@time = time
 	end
 
 	def discount()
-		return 30 if @name == "employee"
-		return 10 if @name == "affiliate"
-		return 5 if @name == "customer" && Time.now-(2*60*60*24*365)>@time 	
+		return 30 if @type == "employee"
+		return 10 if @type == "affiliate"
+		return 5 if @type == "customer" && Time.now-(2*60*60*24*365)>@time 	
 		0
 	end
 end
